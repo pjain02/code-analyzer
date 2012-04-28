1eeb52d0ddbc945b624d83ffbde1b3602f2b2dc0
Prateek Jain <prateek.ja@gmail.com>
2012-04-27 21:45:57 -0400

    Added Tokenizer with a simple splitter. Also added the beasic test case for this function.

---
+++ Analyzer.UnitTest/SplitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Analyzer;
+
+namespace Analyzer.UnitTest
+{
+    [TestClass]
+    public class SplitTest
+    {
+        List<String> inputs;
+        List<String[]> outputs;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            inputs = new List<string>();
+            outputs = new List<string[]>();
+
+            inputs.Add("using Lss.Helpers;");
+            string[] strs = {"using", " ", "Lss", ".", "Helpers", ";"};
+            outputs.Add(strs);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(outputs[0], Tokenizer.Split(inputs[0]));
+        }
+    }
+}
---
+++ Analyzer/Tokenizer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyzer
+{
+    public static class Tokenizer
+    {
+        /// <summary>
+        /// Split the given line into various tokens
+        /// </summary>
+        /// <param name="str">The string to be tokenized</param>
+        /// <returns>An array of string tokens</returns>
+        public static string[] Split(string str)
+        {
+            string[] finalTokens = {"test", "test"};
+            List<String> initialTokens = new List<string>();
+            StringBuilder token = new StringBuilder();
+            foreach (char c in str)
+            {
+                if (c == ' ')
+                {
+                    initialTokens.Add(token.ToString());
+                    initialTokens.Add(" ");
+                    token.Clear();
+                }
+                else if (c == '.')
+                {
+                    initialTokens.Add(token.ToString());
+                    initialTokens.Add(".");
+                    token.Clear();
+                }
+                else if (c == ';')
+                {
+                    initialTokens.Add(token.ToString());
+                    initialTokens.Add(";");
+                    token.Clear();
+                }
+                else if (c == '(')
+                {
+                    initialTokens.Add(token.ToString());
+                    initialTokens.Add("(");
+                    token.Clear();
+                }
+                else if (c == ')')
+                {
+                    initialTokens.Add(token.ToString());
+                    initialTokens.Add(")");
+                    token.Clear();
+                }
+                else if (c == '{')
+                {
+                    initialTokens.Add(token.ToString());
+                    initialTokens.Add("{");
+                    token.Clear();
+                }
+                else if (c == '}')
+                {
+                    initialTokens.Add(token.ToString());
+                    initialTokens.Add("}");
+                    token.Clear();
+                }
+                else if (c == '[')
+                {
+                    initialTokens.Add(token.ToString());
+                    initialTokens.Add("[");
+                    token.Clear();
+                }
+                else if (c == ']')
+                {
+                    initialTokens.Add(token.ToString());
+                    initialTokens.Add("]");
+                    token.Clear();
+                }
+                else
+                    token.Append(c);
+            }
+
+            return initialTokens.ToArray();
+        }
+    }
+}
